07db3c8129fec5940d46608c3e27df0537ad8322
Maikon <maikon.ferreirayt@gmail.com>
2021-12-03 12:29:40 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Maikon Ferreira on 03/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
